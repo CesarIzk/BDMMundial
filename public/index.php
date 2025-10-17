bc2a7cd37f4b952c94fe3326dfbc4c73c97df172
CesarIzk <125629498+CesarIzk@users.noreply.github.com>
2025-10-17 01:13:19 -0600

    logs

--- public/index.php
+++ public/index.php
@@ -2,6 +2,11 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+if (getenv('APP_ENV') !== 'production') {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
 
 session_start();
 const BASE_PATH = __DIR__.'/../';
