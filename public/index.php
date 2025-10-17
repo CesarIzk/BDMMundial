60d8245bd27f336bb2bd549e9e374fe4a054e0a2
CesarIzk <125629498+CesarIzk@users.noreply.github.com>
2025-10-17 01:17:11 -0600

    core

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@ spl_autoload_register(function ($class) {
 
 //require base_path('bootstrap.php');
 
-$router = new \Core\Router();
+$router = new \core\Router();
 $routes = require base_path('routes.php');
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
